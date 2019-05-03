8c0e4441d581309b57661202b40c167e44619509
tabluw <luming.wang@rwwa.com.au>
2019-05-03 15:05:53 +0800

    Updates

---
+++ precache-manifest.e99e83b0462c8704855ee69b0f76ddab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "843194a9a334a4104f2e1fa49e5dc8e9",
+    "url": "/appsync-test/index.html"
+  },
+  {
+    "revision": "7bfa43c2dd6d52e802cb",
+    "url": "/appsync-test/static/css/main.3e545749.chunk.css"
+  },
+  {
+    "revision": "d11242fb0c4dd8cd7b9c",
+    "url": "/appsync-test/static/js/2.4723e843.chunk.js"
+  },
+  {
+    "revision": "7bfa43c2dd6d52e802cb",
+    "url": "/appsync-test/static/js/main.883c081f.chunk.js"
+  },
+  {
+    "revision": "4280995e9ec2d82b8071",
+    "url": "/appsync-test/static/js/runtime~main.6ed4e500.js"
+  }
+]);
