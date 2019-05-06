ce69e6530d9f47cdea401ec796c653579ddf4fe1
leapm <graphql.js@gmail.com>
2019-05-06 22:21:42 +0800

    converted to subject

--- src/PushData/index.ts
+++ src/PushData/index.ts
@@ -3,7 +3,8 @@ import gql from 'graphql-tag'
 import ZenObservable from 'zen-observable-ts'
 import { onEventDomainChange } from './graphql/subscriptions'
 import { Observable } from 'apollo-client/util/Observable'
-import { Subject } from 'rxjs'
+import * as Rx from 'rx'
+import { string } from 'prop-types'
 interface MakeObservableReq {
   eventDomain?: string
   eventType?: string
@@ -15,103 +16,64 @@ interface SubscriptionMeta<T> {
   children: ZenObservable.Subscription[]
   observer: ZenObservable.Observer<T>
 }
+
+interface ConnectionState {
+  subscriptionCount: number
+  nativeSubscription: ZenObservable.Subscription | null
+}
 function createFailedObs<T>(error: any) {
   let obs = new Observable<T>(observer => observer.error(error))
   return obs
   // return createRxSubjectFromZenObservable<T>(obs)
 }
 
-function createRxSubjectFromZenObservable<T>(
-  obs: ZenObservable<T>
-): Subject<T> {
-  let subject = new Subject<T>()
-  debugger
-  obs.subscribe(subject)
+function subscribeToSubject<T>(
+  subject: Rx.Subject<T>,
+  creator: () => ZenObservable<T>,
+  currentState: ConnectionState
+) {
+  let obs = creator()
+  let nativeSubscription = obs.subscribe(
+    data => {
+      subject.onNext(data)
+    },
+    error => {
+      nativeSubscription.unsubscribe()
+      subscribeToSubject(subject, creator, currentState)
+    },
+    () => subject.onCompleted()
+  )
+
+  currentState.nativeSubscription = nativeSubscription
+}
+function decorateObservable<T>(creator: () => ZenObservable<T>): Rx.Subject<T> {
+  let subject = new Rx.Subject<T>()
+  let currentState: ConnectionState = {
+    subscriptionCount: 0,
+    nativeSubscription: null
+  }
+  subscribeToSubject(subject, creator, currentState)
+
+  let subjectSubscribe = subject.subscribe.bind(subject)
+
   return subject
 }
 
-function decorateObserverWithReconnectHandler<T>(
-  observer: ZenObservable.Observer<T>,
-  creator: Function,
-  subscriptions: ZenObservable.Subscription[]
-) {
-  return {
-    ...observer,
-    error: (err: any) => {
-      console.error('error happend with network', err)
-      if (err.errorMessage == 'AMQJSC0000I OK.') {
-        setTimeout(() => {
-          let obs = decorateObservable<T>(creator)
-          subscriptions.push(obs.subscribe(observer))
-        }, 1000)
-      } else if (observer.error) {
-        observer.error(err)
-      }
-    },
-  }
-}
-
-function decorateObservable<T>(creator: Function): ZenObservable<T> {
-  let obs = creator()
-  let nativeSubscribe = obs.subscribe
-  let subscribe = function(
-    observerOrNext: ((value: T) => void) | ZenObservable.Observer<T>,
-    error?: (error: any) => void,
-    complete?: () => void
-  ) {
-    let observer =
-      typeof observerOrNext == 'function'
-        ? {
-            next: observerOrNext,
-            error,
-            complete,
-          }
-        : observerOrNext
-    let shaddowSubscriptions: ZenObservable.Subscription[] = []
-    observer = decorateObserverWithReconnectHandler(
-      observer,
-      creator,
-      shaddowSubscriptions
-    )
-    const nativeSubscription = nativeSubscribe.apply(obs, [observer])
-    const nativeUnsubscribe = nativeSubscription.unsubscribe
-
-    nativeSubscription.unsubscribe = () => {
-      console.log('call unsubscribe')
-      shaddowSubscriptions.forEach(s => {
-        console.log('call children subscribe')
-        s.unsubscribe()
-      })
-      shaddowSubscriptions = []
-      nativeUnsubscribe.apply(nativeSubscription)
-    }
-    return nativeSubscription
-  }
-
-  obs.subscribe = subscribe
-  return obs
-}
-
-function makeObservable<T>(req: MakeObservableReq): Promise<ZenObservable<T>> {
+function makeObservable<T>(req: MakeObservableReq): Promise<Rx.Subject<T>> {
   // Add logic to support other subscription
   const { eventDomain, eventType, eventId } = req
-  return initClient()
-    .then(client => {
-      let creator = () => {
-        let obs = client!.subscribe({
-          query: gql(onEventDomainChange),
-          variables: { eventDomain },
-        })
-        return obs
-      }
-      let obs = decorateObservable<{ data: T }>(creator)
-      let newObs = obs.map<T>(val => val.data)
-      return newObs
-      //return createRxSubjectFromZenObservable<T>(newObs)
-    })
-    .catch(error => {
-      return createFailedObs<T>(error)
-    })
+  return initClient().then(client => {
+    let creator: () => ZenObservable<T> = () => {
+      let obs = client!.subscribe({
+        query: gql(onEventDomainChange),
+        variables: { eventDomain }
+      })
+      return obs
+    }
+
+    return decorateObservable<T>(creator)
+    //return createRxSubjectFromZenObservable<T>(newObs)
+  })
 }
 
 export default makeObservable
